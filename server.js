7e758b246a8840cbcf9713d024dba5661db788ad
Dustin1904 <dustin04ms@gmail.com>
2024-11-05 16:47:17 -0500

    error

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect(process.env.MONGO_uri,{
+mongoose.connect(process.env.MONGO_URI,{
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
